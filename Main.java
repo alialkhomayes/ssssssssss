fd96353d63a565988ca68873494c3ead7307b1b3
alialkhomayes <115029632+alialkhomayes@users.noreply.github.com>
2023-02-09 17:31:46 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+package src;
+
+
+
+import java.io.IOException;
+import java.text.ParseException;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.print("hello world");
+	}
+
+}
+
+
+
